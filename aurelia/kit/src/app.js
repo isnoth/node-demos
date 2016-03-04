bd50334d5b3347236b954644d725443ad36a531e
yao.lin <yao.lin@nokia.com>
2016-03-04 17:03:16 +0800

    users.html

--- aurelia/kit/src/app.js
+++ aurelia/kit/src/app.js
@@ -1,13 +1,11 @@
-export class Welcome {
-  heading = 'Welcome to Aurelia!';
-  firstName = 'John';
-  lastName = 'Doe';
+export class App {
+  configureRouter(config, router){
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['','welcome'],  name: 'welcome',  moduleId: './welcome',  nav: true, title:'Welcome' },
+      { route: 'users',         name: 'users',    moduleId: './users',    nav: true, title:'Github Users' }
+    ]);
 
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-
-  submit() {
-    alert(`Welcome, ${this.fullName}!`);
+    this.router = router;
   }
 }
