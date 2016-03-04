d39bd456d3aa98a98531000dd13510d175b87451
yao.lin <yao.lin@nokia.com>
2016-03-04 16:14:40 +0800

    test submit

--- aurelia/kit/src/app.js
+++ aurelia/kit/src/app.js
@@ -1,3 +1,13 @@
-export class App {
-  message = 'Welcome to Aurelia!';
+export class Welcome {
+  heading = 'Welcome to Aurelia!';
+  firstName = 'John';
+  lastName = 'Doe';
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  submit() {
+    alert(`Welcome, ${this.fullName}!`);
+  }
 }
