29a9165d347ba3c1aaa17e56710bfa5835e73a8f
yao.lin <yao.lin@nokia.com>
2016-03-11 16:24:34 +0800

    test socket.io step1

---
+++ socket.io/chatApp/index.js
@@ -0,0 +1,10 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(8888, function(){
+  console.log('listening on *:8888');
+});
